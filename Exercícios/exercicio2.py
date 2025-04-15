ab8e494e11808316d78317cb3dd977a280ac885f
DanielSoarPT <danielsoares201505@gmail.com>
2025-04-15 12:41:22 +0100

    Exercise2Changes

--- Exercícios/exercicio2.py
+++ Exercícios/exercicio2.py
@@ -1,7 +1,6 @@
 num = int(input("Insira um numero entre 1 a 13: "))
 
-if num % num == 0 and num % 2 != 0 and num % +
-3 != 0:
-    print("numero primo")
+if num % num == 0 and num % 2 != 0 and num % + 3 != 0:
+    print("É um número primo")
 else:
-    print("não é um numero primo")
+    print("Não é um número primo")
